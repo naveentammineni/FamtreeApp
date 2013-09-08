5491ede5198e7efbc4f6295a675d1a82721c9287
Naveen Tammineni <tammineni.naveen@gmail.com>
2013-09-08 08:46:15 -0400

    Testing the Gestures

---
+++ FamtreeApp/src/com/example/famtreeapp/GestureActivity.java
@@ -0,0 +1,61 @@
+package com.example.famtreeapp;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGestureListener;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
+import android.graphics.Color;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class GestureActivity extends Activity implements OnGesturePerformedListener{
+	GestureLibrary   gestureLib;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gesture);
+		GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
+		View inflate = getLayoutInflater().inflate(R.layout.activity_gesture, null);
+		gestureOverlayView.setGestureColor(Color.rgb(0, 255, 0));
+		gestureOverlayView.setGestureVisible(true);
+		gestureOverlayView.setUncertainGestureColor(Color.rgb(255, 0, 0));
+		gestureOverlayView.addView(inflate);
+		gestureOverlayView.addOnGesturePerformedListener(this);
+		
+	    gestureLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.gesture, menu);
+		return true;
+	}
+
+	@Override
+	public void onGesturePerformed(GestureOverlayView arg0, Gesture g) {
+		ArrayList<Prediction> predictions = gestureLib.recognize(g); 
+		if(predictions.size() >0 ){
+			Prediction prediction = predictions.get(0);
+			if(prediction.score >1){
+				String s = prediction.name;
+				if(s.equals("UP") || s.equals("up"))
+					Toast.makeText(getApplicationContext(), "UP", 1).show();
+				else if(s.equals("DOWN") || s.equals("down"))
+					Toast.makeText(getApplicationContext(), "DOWN", 1).show();
+			}
+		}
+		
+	}
+
+}
--- FamtreeApp/src/com/example/famtreeapp/MainActivity.java
+++ FamtreeApp/src/com/example/famtreeapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.famtreeapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,10 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    public void openGestures(View view){
+    	Intent intent = new Intent(this,GestureActivity.class);
+    	startActivity(intent);
     }
     
 }
